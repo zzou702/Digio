96c68011a84c44ee4bfe8b841fe1aa39f7cf6c4d
ochu761 <ochu761@aucklanduni.ac.nz>
2022-08-26 06:05:36 +1200

    renamed variables to convention

--- app/src/main/java/com/example/team7_project_1/handlers/MapHandler.java
+++ app/src/main/java/com/example/team7_project_1/handlers/MapHandler.java
@@ -9,34 +9,34 @@ public class MapHandler {
     MapHandler serves as middleware between Application/Activity layer and Persistence/Data layer
      */
 
-    static PhoneMapper phoneMapper;
-    static SpecificationMapper specificationMapper;
-    static LoadingActivity loadingActivity;
+    static PhoneMapper phone_mapper;
+    static SpecificationMapper specification_mapper;
+    static LoadingActivity loading_activity;
 
-    public static void setContextToCallback(LoadingActivity loadingActivity) {
-        MapHandler.loadingActivity = loadingActivity;
+    public static void setContextToCallback(LoadingActivity loading_activity) {
+        MapHandler.loading_activity = loading_activity;
     }
 
     public static void initializePhoneMapper(String collection_path) {
-        phoneMapper = new PhoneMapper(collection_path);
+        phone_mapper = new PhoneMapper(collection_path);
     }
 
     public static void initializeSpecificationMapper(String collection_path) {
-        specificationMapper = new SpecificationMapper(collection_path);
+        specification_mapper = new SpecificationMapper(collection_path);
     }
 
     public static void beginFetch() {
-        phoneMapper.fetchFromDatabase();
-        specificationMapper.fetchFromDatabase();
+        phone_mapper.fetchFromDatabase();
+        specification_mapper.fetchFromDatabase();
     }
 
     /*
     Called by mappers when they have finished fetching
      */
     public static void callbackFromFetch() {
-        if (phoneMapper.isStillFetching() || specificationMapper.isStillFetching()) {
+        if (phone_mapper.isStillFetching() || specification_mapper.isStillFetching()) {
             return;
         }
-        loadingActivity.finishLoading();
+        loading_activity.finishLoading();
     }
 }
